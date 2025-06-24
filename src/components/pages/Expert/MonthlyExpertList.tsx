cd301fc257ce5e0d1025bbf09b469e5cf48ab5f3
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-06-24 10:00:18 +0900

    ✨ [new features] 이달의 엑스퍼트 상단 설명 문구 및 스타일 추가

--- src/components/pages/Expert/MonthlyExpertList.tsx
+++ src/components/pages/Expert/MonthlyExpertList.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import Button from '../../common/Button';
 import ExpertCard from './ExpertCard';
 import { useExpertStore } from '../../../stores/useExpertStore';
+import dayjs from 'dayjs';
+import ScrollContainer from 'react-indiana-drag-scroll';
 
 export default function MonthlyExpertList(): JSX.Element {
   const navigate = useNavigate();
@@ -12,17 +14,29 @@ export default function MonthlyExpertList(): JSX.Element {
     fetchExperts();
   }, [fetchExperts]);
 
+  const currentMonth = dayjs().format('M');
+  const displayMonth = `${currentMonth}월`;
+
   return (
-    <div className="my-2">
-      <div className="flex justify-between p-4">
-        <div className="text-h3">이 달의 엑스퍼트</div>
+    <div>
+      <div className="h-[8px] bg-[#F5F5F5] my-3 w-full" />
+      <div className="flex justify-between items-center px-4 pt-4">
+        <div className="text-h3 font-semibold">이 달의 엑스퍼트</div>
         <Button variant="text" onClick={() => navigate('/experts-list')}>
           더보기
         </Button>
       </div>
-      {experts.map(item => (
-        <ExpertCard expert={item} />
-      ))}
+
+      <p className="text-b2 text-[#9C9C9C] px-4 mt-1 mb-3">
+        {displayMonth} 가장 상담율이 높은 엑스퍼트 5명을 선정했어요.
+      </p>
+
+      <div className="flex flex-col gap-2 px-4">
+        {experts.map(item => (
+          <ExpertCard key={item.id} expert={item} />
+        ))}
+      </div>
+      <ScrollContainer></ScrollContainer>
     </div>
   );
 }
